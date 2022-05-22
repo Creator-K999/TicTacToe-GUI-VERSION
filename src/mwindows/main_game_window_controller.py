e84b8815c4284a374cad369c84df35dc0572f709
AMZ <ahmedzaki.bm@gmail.com>
2022-05-22 18:08:39 +0300

    My third commit

--- src/mwindows/main_game_window_controller.py
+++ src/mwindows/main_game_window_controller.py
@@ -1,11 +1,8 @@
-from _thread import start_new_thread
-
 from PyQt6 import uic, QtWidgets
 from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtWidgets import QMainWindow
 
 from src.gprocessing import main_game_processing
-from src.subwindows.timer_window_controller import TimerWindow
 
 
 class MainGameWindow(QMainWindow):
@@ -29,26 +26,13 @@ class MainGameWindow(QMainWindow):
             for i in range(1, 3)
         }
 
-        self.__timer_buttons = {
-            "b10": self.findChild(QtWidgets.QPushButton, "b10"),
-            "b11": self.findChild(QtWidgets.QPushButton, "b11")
-        }
-
-        self.__timer_label = self.findChild(QtWidgets.QLabel, "timer_label_object")
-
-        if None in self.__buttons.values() or \
-                None in self.__player_labels.values() or \
-                None in self.__timer_buttons.values() or \
-                None is self.__timer_label:
+        if None in self.__buttons.values() or None in self.__player_labels.values():
             print("One or more button's couldn't be found!")
 
         self.__game_processor = main_game_processing.MainGameProcessing(self)
 
         for button in self.__buttons.values():
             button.clicked.connect(self.__buttonPress)
-
-        self.__timer_buttons["b10"].clicked.connect(lambda: start_new_thread(self.__game_processor.prepare_timer, ()))
-        self.__timer_buttons["b11"].clicked.connect(lambda: TimerWindow(self.__game_processor))
 
     @property
     def game_processor(self):
@@ -65,10 +49,6 @@ class MainGameWindow(QMainWindow):
     @property
     def player_labels(self):
         return self.__player_labels
-
-    @property
-    def timer_label(self):
-        return self.__timer_label
 
     def __buttonPress(self):
         result = self.__game_processor.button_clicked_process(self.sender())
