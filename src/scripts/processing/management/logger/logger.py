6a94f706be153ab86409f34f94d8d17245fdbb3e
AMZ <ahmedzaki.bm@gmail.com>
2022-06-16 07:13:58 +0300

    - Made a "custom_stack" option in logger and gave it a None as a default value.

--- src/scripts/processing/management/logger/logger.py
+++ src/scripts/processing/management/logger/logger.py
@@ -15,43 +15,60 @@ class Log:
         raise NotImplementedError("Log cannot be instantiated!")
 
     @staticmethod
-    def __log(target, message):
-        caller_info = stack()[2]  # list of named tuples
+    def __log(target, message, custom_stack):
         Thread(
             daemon=True,
             target=target,
-            args=(f"\n\tFILE: {basename(caller_info.filename)}"
-                  f"\n\tFUNC: {caller_info.function}"
-                  f"\n\tLINE: {caller_info.lineno}"
+            args=(f"\n\tFILE: {basename(custom_stack.filename)}"
+                  f"\n\tFUNC: {custom_stack.function}"
+                  f"\n\tLINE: {custom_stack.lineno}"
                   f"\n\tMESSAGE: {message}\n",)
         ).start()
 
     @classmethod
-    def debug(cls, message):
+    def debug(cls, message, custom_stack=None):
         with cls.__lock:
-            cls.__log(cls.__logger.debug, message)
+            if custom_stack is None:
+                cls.__log(cls.__logger.debug, message, stack()[1])
+            else:
+                cls.__log(cls.__logger.debug, message, custom_stack)
 
     @classmethod
-    def info(cls, message):
+    def info(cls, message, custom_stack=None):
         with cls.__lock:
-            cls.__log(cls.__logger.info, message)
+            if custom_stack is None:
+                cls.__log(cls.__logger.info, message, stack()[1])
+            else:
+                cls.__log(cls.__logger.info, message, custom_stack)
 
     @classmethod
-    def warning(cls, message):
+    def warning(cls, message, custom_stack=None):
         with cls.__lock:
-            cls.__log(cls.__logger.warning, message)
+            if custom_stack is None:
+                cls.__log(cls.__logger.warning, message, stack()[1])
+            else:
+                cls.__log(cls.__logger.warning, message, custom_stack)
 
     @classmethod
-    def error(cls, message):
+    def error(cls, message, custom_stack=None):
         with cls.__lock:
-            cls.__log(cls.__logger.error, message)
+            if custom_stack is None:
+                cls.__log(cls.__logger.error, message, stack()[1])
+            else:
+                cls.__log(cls.__logger.error, message, custom_stack)
 
     @classmethod
-    def exception(cls, message):
+    def exception(cls, message, custom_stack=None):
         with cls.__lock:
-            cls.__log(cls.__logger.exception, message)
+            if custom_stack is None:
+                cls.__log(cls.__logger.exception, message, stack()[1])
+            else:
+                cls.__log(cls.__logger.exception, message, custom_stack)
 
     @classmethod
-    def critical(cls, message):
+    def critical(cls, message, custom_stack=None):
         with cls.__lock:
-            cls.__log(cls.__logger.critical, message)
+            if custom_stack is None:
+                cls.__log(cls.__logger.critical, message, stack()[1])
+            else:
+                cls.__log(cls.__logger.critical, message, custom_stack)
