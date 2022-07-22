533fd0c88f65c1f575c940223d280b711c7b53fc
AMZ <ahmedzaki.bm@gmail.com>
2022-07-22 14:06:41 +0300

    - Slightly enhanced `ObjectsManager` class.

--- src/scripts/processing/management/objects/objects_manager.py
+++ src/scripts/processing/management/objects/objects_manager.py
@@ -10,7 +10,6 @@ class ObjectsManager:
 
         Log.info(f"Trying to get {object_name} instance...")
 
-        _object = None
         repr_object_name = None
 
         try:
@@ -19,11 +18,12 @@ class ObjectsManager:
 
         except KeyError:
             Log.exception(f"{repr_object_name} doesn't exist!")
-            return None
 
         except AttributeError as E:
             Log.exception(f"_object is not a class! - {E}")
-            return None
+
+        except Exception as E:
+            Log.error(f"ERROR: {E}")
 
         else:
             Log.debug(f"Found {repr_object_name}")
@@ -31,10 +31,10 @@ class ObjectsManager:
 
     @classmethod
     def destruct_objects(cls):
-        leak_found = False
+        leak_found = len(cls.__objects) != 0
+
         Log.debug("Looking for memory leaks!")
-        for _object in [*cls.__objects]:
-            leak_found = True
+        for _object in (*cls.__objects,):
             Log.warning(f"Found a memory leak!, object is {_object}")
             del cls.__objects[_object]
 
@@ -52,33 +52,37 @@ class ObjectsManager:
         try:
             object_name = custom_name if custom_name else _object.__name__
             repr_object_name = f"'{object_name}'"
+
+            if object_name in cls.__objects:
+                raise ValueError("Object Already Exist!")
+
             cls.__objects[object_name] = _object(*args, **kwargs)
 
-        except AttributeError:
-            Log.exception(f"object '{_object}' is not a class!")
-            return None
+        except AttributeError as E:
+            Log.exception(f"ERROR: {E}")
+
+        except ValueError as E:
+            Log.error(f"ERROR: {E}")
 
         except Exception as E:
-            Log.exception(f"Some Error occurred while creating object {repr_object_name}!{E}")
-            return None
+            Log.exception(f"Some Error occurred while creating object {repr_object_name}!\n\tERROR: {E}")
 
-        Log.debug(f"Successfully created Object {repr_object_name}!")
-        return cls.__objects[object_name]
+        else:
+            Log.debug(f"Successfully created Object {repr_object_name}!")
+            return cls.__objects[object_name]
 
     @classmethod
     def delete_object(cls, object_name):
 
-        Log.info(f"Trying to delete {object_name} instance...!")
-        repr_object_name = repr(object_name)
+        Log.info(f"Trying to delete {object_name} object...!")
+        repr_object_name = f"'{object_name}'"
 
         try:
             del cls.__objects[object_name]
+            Log.debug(f"Successfully deleted {repr_object_name}")
 
         except KeyError:
             Log.exception(f"{repr_object_name} doesn't exist!")
 
         except Exception as E:
             Log.exception(f"Error happened!{E}")
-
-        else:
-            Log.debug(f"Successfully deleted {repr_object_name}")
