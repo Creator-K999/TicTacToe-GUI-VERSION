9773e978500cefdf5eed2af0c82094c85285193e
AMZ <ahmedzaki.bm@gmail.com>
2022-09-02 17:55:54 +0300

    Continue working on account_deletion_controller.py
    Signed-off-by: AMZ <ahmedzaki.bm@gmail.com>

--- src/windows/subw/account_deletion_controller.py
+++ src/windows/subw/account_deletion_controller.py
@@ -9,19 +9,6 @@ from src import Log
 
 class AccountDeletion(QDialog):
 
-    __account_names = None
-
-    # move the condition into re_connect method.
-    if not DBManager.is_open():
-        DBManager.re_connect()
-
-    db = DBManager.db()
-
-    data = db.execute("SELECT Name FROM Credentials").fetchall()
-
-    if data:
-        __account_names = {name[0] for name in data}
-
     def __init__(self):
         super().__init__()
 
@@ -37,11 +24,33 @@ class AccountDeletion(QDialog):
 
         self.__account_delete_button = self.findChild(QPushButton, "account_delete_button")
 
+        # move the condition into re_connect method.
+        if not DBManager.is_open():
+            DBManager.re_connect()
+
+        self.__db = DBManager.db()
+        data = self.__db.execute("SELECT Name FROM Credentials").fetchall()
+        self.__accounts_names = {name[0] for name in data}
+
         model = QStandardItemModel()
         self.__accounts_list_view.setModel(model)
 
-        for name in AccountDeletion.__account_names:
+        for name in self.__accounts_names:
             model.appendRow(QStandardItem(name))
+
+        self.__old_key_press_event = self.__search_entry.keyPressEvent
+        self.__search_entry.keyPressEvent = self.__on_search
+
+    def __on_search(self, event):
+        self.__old_key_press_event(event)
+        text = self.__search_entry.text().lower()
+
+        model = QStandardItemModel()
+        self.__accounts_list_view.setModel(model)
+
+        for name in self.__accounts_names:
+            if name.lower().startswith(text):
+                model.appendRow(QStandardItem(name))
 
     def closeEvent(self, event) -> None:
         Log.debug("Closing Settings Window...")
